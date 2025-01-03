ac7172db8c674a9c6527cbaed46602fa060a7215
DavidAndBar <davidbar22@hotmail.com>
2025-01-02 23:35:00 -0500

    Summarizes all the connection to the db using only mongoose

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require('express');
+const cors = require('cors')
 const index = require('./to-do-list-react/index');
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT_TODO || 3000;
 
 const app = express();
 
-
-
+// Define CORS options:
+var corsOptions = {
+    origin: '*',
+    exposedHeaders: '*'
+}
 
 // Define the url used with a router inside backtest file. - These are middlewares.
 app.use(express.json()) // This middleware is to accept json in the body request.
 
-app.use('/to-do-list',index);
+app.use('/to-do-list',cors(corsOptions),index);
 
 // Executes the back
 app.listen(port, () => {
     console.log(`Server Running in port: ${port}`);
-}); 
+});
--- to-do-list-react/index.js
+++ to-do-list-react/index.js
@@ -1,72 +1,74 @@
 const express = require('express');
-const client = require('./databases/conexion_to_do');
 const router = express.Router();
+const db = require('./databases/conexion_to_do')
 const Users = require('./models/toDo/users');
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
-const db_collection = client.db('to_do_react').collection('users')
-
-// Landing page
+// Landing
 router.get('/',(request, response)=>{
     response.json({'message': 'This is the landing page for the to-do-list backend'})
 })
-
+//Register page
 router.post('/register', async (req, res) => {
-    const email = req.body.email || "";
-    await client.connect()
-    if (!!email){
-        await db_collection.findOne({"email": email})
-        .then(async result => {
-            if (result === null) {
-                const user = new Users(req.body);
-                const salt = await bcrypt.genSalt(parseInt(process.env.HASH_TODO));
-                const password = await bcrypt.hash(req.body.password, salt);
-                user.password = password
-                
-                db_collection.insertOne(user)
-                .then(user => res.json({message: true}))
-                .catch(error => res.json(error));
-            }
-            else {
-                res.json({message: false});
-            }
-
-        })
-        .catch(error => res.json({error: error}));
-    } else {
+    try {
+        const email = req.body.email || "";
+        if (!!email){
+            Users.find({"email": email})
+            .then(async result => {
+                if (result === null) {
+                    const user = new Users(req.body);                    
+                    const salt = await bcrypt.genSalt(parseInt(process.env.HASH_TODO));
+                    const password = await bcrypt.hash(req.body.password, salt);
+                    user.password = password;
+                    user.save()
+                    .then(user => res.json({message: true}))
+                    .catch(error => res.json(error));
+                }
+                else {
+                    res.json({message: false});
+                }
+            })
+            .catch(error => res.json({error: error}));
+        } else {
+            res.json({message: false})
+        }
+    } catch {
         res.json({message: false})
     }
 })
 
 router.post('/login', async (req, res) => {
     const user = req.body;
-    await client.connect()
-    
-    db_collection.findOne({email: user.email})
-    .then(result => 
-        { if (result != null) {
-            bcrypt.compare(user.password, result.password)
-            .then(comp => {
-                if (comp) {
-                    const token = jwt.sign({
-                            id: result._id
-                        }, process.env.SECRET_TOKEN_TODO, {expiresIn : "1 year"});
-                    
-                    res.header('auth-token', token)
-                    .json({"message": comp});
-                } else {
-                    res.json({"message": comp})
-                }
-            })
-            .catch(error => res.json({"message": error}))
-        } else {
-            res.json({"message": false})
-        }
-    })
-    .catch(error => res.json({message: error}));
-
+    try {
+        Users.findOne({email: user.email})
+        .then(result => 
+            {if (result != null) {
+                bcrypt.compare(user.password, result.password)
+                .then(comp => {
+                    if (comp) {
+                        const token = jwt.sign({
+                                id: result._id
+                            }, process.env.SECRET_TOKEN_TODO, {expiresIn : "1 year"});
+                        res.header('auth-token', token)
+                        .json({"message": comp});
+                    } else {
+                        res.json({"message": comp});
+                    }
+                })
+                .catch(error => res.json({"message": error}))
+            } else {
+                res.json({"message": false})
+            }
+        })
+        .catch(error => {
+            res.json({message: error})
+        });
+    } catch{
+        res.json({"message": "Error in the try"})
+    } 
 })
 
 module.exports = router;
