4a370eb7e6619ca3c80e6abdfb5777d4728ff19f
DavidAndBar <davidbar22@hotmail.com>
2025-01-03 23:08:23 -0500

    fixes some minor errors in register and sign in logic

--- to-do-list-react/index.js
+++ to-do-list-react/index.js
@@ -15,10 +15,11 @@ router.get('/',(request, response)=>{
 router.post('/register', async (req, res) => {
     try {
         const email = req.body.email || "";
+        
         if (!!email){
-            Users.find({"email": email})
+            Users.findOne({"email": email})
             .then(async result => {
-                if (result === null) {
+                if (!!!result) {
                     const user = new Users(req.body);                    
                     const salt = await bcrypt.genSalt(parseInt(process.env.HASH_TODO));
                     const password = await bcrypt.hash(req.body.password, salt);
@@ -46,13 +47,14 @@ router.post('/login', async (req, res) => {
         Users.findOne({email: user.email})
         .then(result => 
             {if (result != null) {
+                const username = result.username;
                 bcrypt.compare(user.password, result.password)
                 .then(comp => {
                     if (comp) {
                         const token = jwt.sign({
                                 id: result._id
                             }, process.env.SECRET_TOKEN_TODO, {expiresIn : "1 year"});
-                        res.header('auth-token', token)
+                        res.header('auth-token', token).header('username', username)
                         .json({"message": comp});
                     } else {
                         res.json({"message": comp});
@@ -69,6 +71,16 @@ router.post('/login', async (req, res) => {
     } catch{
         res.json({"message": "Error in the try"})
     } 
+})
+
+router.post('/check-token', async (req, res) => {
+    const userToken = req.body.token || "";
+    try {
+        const verified = jwt.verify(userToken, process.env.SECRET_TOKEN_TODO);
+        res.json({validToken: verified.id})
+    } catch(err) {
+        res.json({validToken: false})
+    }
 })
 
 module.exports = router;
